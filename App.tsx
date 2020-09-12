f3f80589b9aed8a3fe3d5dc835af57a7978ad106
Farzin Mirzaie <farzin.mirzaie@gmail.com>
2020-09-12 20:03:12 +0430

    Playground screen

--- App.tsx
+++ App.tsx
@@ -1,21 +1,10 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { Playground } from "./src/screens";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+const App = () => (
+  <>
+    <Playground />
+  </>
+);
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
